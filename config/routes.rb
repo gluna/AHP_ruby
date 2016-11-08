abce87dbee1c47ad9fe417cdbf2cf1689a5c8e3e
gluna <gllouvem@gmail.com>
2016-11-08 17:32:37 -0200

    inclusao bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :grupo_criterios
   resources :criterios
   resources :alternativas
+
+  root 'projetos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
