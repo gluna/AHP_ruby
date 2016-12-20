b32fe5c097f508563085e21948561fa3eb3885f5
gluna <gllouvem@gmail.com>
2016-12-19 22:27:56 -0200

    config db

--- db/migrate/20161129104820_create_julgamento_criterios.rb
+++ db/migrate/20161129104820_create_julgamento_criterios.rb
@@ -2,8 +2,8 @@ class CreateJulgamentoCriterios < ActiveRecord::Migration[5.0]
   def change
     create_table :julgamento_criterios do |t|
       t.belongs_to :projeto, foreign_key: true
-      t.belongs_to :criterio_1, foreign_key: true
-      t.belongs_to :criterio_2, foreign_key: true
+      t.belongs_to :criterio_1
+      t.belongs_to :criterio_2
       t.float :valor
 
       t.timestamps
