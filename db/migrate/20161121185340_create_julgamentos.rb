68e4eb18d7390366c29a071e9079606db69307c7
gluna <gllouvem@gmail.com>
2016-12-19 22:11:23 -0200

    config db

--- db/migrate/20161121185340_create_julgamentos.rb
+++ db/migrate/20161121185340_create_julgamentos.rb
@@ -4,8 +4,8 @@ class CreateJulgamentos < ActiveRecord::Migration[5.0]
       t.belongs_to :usuario, foreign_key: true
       t.belongs_to :projeto, foreign_key: true
       t.belongs_to :criterio, foreign_key: true
-      t.belongs_to :alternativa_1, class_name: "Alternativa", foreign_key: true
-      t.belongs_to :alternativa_2, class_name: "Alternativa", foreign_key: true
+      t.belongs_to :alternativa_1
+      t.belongs_to :alternativa_2
       t.float :valor
 
       t.timestamps
