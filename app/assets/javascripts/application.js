6c3aeb8946469d0f58705769735ed42537e31ed9
gluna <gllouvem@gmail.com>
2017-08-22 22:23:21 -0300

    preenchimento automatico

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,17 @@
 //= require turbolinks
 //= require jquery_ujs
 //= require_tree .
-(function set_value(){
-  alert("teste");
+function set_value(element){
+  var id = element.id.substring(0,element.id.indexOf("_"))+element.id.substring(element.id.lastIndexOf("_"),element.id.length)+element.id.substring(element.id.indexOf("_"),element.id.lastIndexOf("_"));
+  if(element.id.substring(element.id.lastIndexOf("_"),element.id.length) != element.id.substring(element.id.indexOf("_"),element.id.lastIndexOf("_"))){
+    var x = document.getElementById(id);
+    var num = 1/element.value;
+    var num2 = Math.trunc(num*1000)/1000;
+    if(num2 > 1){
+      num2 = num2.toFixed(1);
+    }
+    x.value = num2;
+  }else{
+    element.value = "1.0";
+  }
  }
-).
